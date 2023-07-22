ac1bb075ae9c9a8214ca658e36295c395d4c1636
Robert MacWha <trebor.ahwcam@gmail.com>
2023-07-22 11:22:02 -0200

    Extenion fix

--- extension/content.js
+++ extension/content.js
@@ -1,58 +1,63 @@
-window.onload = function() {
-    // Create a new observer
-    let observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        // Check if new nodes are added
-        if (mutation.addedNodes) {
-          mutation.addedNodes.forEach((node) => {
-            // Only handle element nodes
-            if (node.nodeType === 1) {
-              // Check if the node is an image
-              if (node.tagName === 'IMG') {
-                // Handle the image
-                handleImage(node);
-              }
+window.onload = function () {
+  // Create a new observer
+  let observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      // Check if new nodes are added
+      if (mutation.addedNodes) {
+        mutation.addedNodes.forEach((node) => {
+          // Only handle element nodes
+          if (node.nodeType === 1) {
+            // Check if the node is an image
+            if (node.tagName === 'IMG') {
+              // Handle the image
+              handleImage(node);
             }
-          });
-        }
-      });
-    });
-  
-    // Start observing the document with the configured parameters
-    observer.observe(document, { childList: true, subtree: true });
-  
-    function handleImage(img) {
-        console.log("Loading Data:");
-        // Create a new Image object
-        let image = new Image();
-        // Set the src of the image
-        image.src = img.src;
-        // Wait for the image to load
-        image.onload = function() {
-          // Create a new canvas
-          let canvas = document.createElement('canvas');
-          // Set the width and height of the canvas
-          canvas.width = image.width;
-          canvas.height = image.height;
-          // Get the 2D context of the canvas
-          let context = canvas.getContext('2d');
-          // Draw the image onto the canvas
-          context.drawImage(image, 0, 0);
-          // Get the image data from the canvas
-          let imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-          // Get the pixel data from the image data
-          let pixelData = imageData.data;
-          // Create a SHA-256 hash of the pixel data
-          window.crypto.subtle.digest('SHA-256', pixelData)
-            .then(hashBuffer => {
-              // Convert the hash to a string
-              const hashArray = Array.from(new Uint8Array(hashBuffer));
-              const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-              console.log("Loading Data:" + hashHex);
-            })
-            .catch(e => {
-              console.log('There has been a problem with your fetch operation: ' + e.message);
-            });
-        }
+          }
+        });
       }
-    }      
+    });
+  });
+
+  // Start observing the document with the configured parameters
+  observer.observe(document, { childList: true, subtree: true });
+
+  function handleImage(img) {
+    // Create a new Image object
+    let image = new Image();
+    // Set the src of the image
+    image.src = img.src;
+    fetch(image.src)
+      .then(response => response.blob())
+      .then(blob => createImageBitmap(blob))
+      .then(imageBitmap => {
+        // Create a canvas
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+
+        // Set the canvas size to the image size
+        canvas.width = imageBitmap.width;
+        canvas.height = imageBitmap.height;
+
+        // Draw the image onto the canvas
+        ctx.drawImage(imageBitmap, 0, 0);
+
+        // Get the pixel data from the canvas
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const uint8Array = new Uint8Array(imageData)
+
+        // Call the function and get the SHA-256 hash
+        sha256(uint8Array).then(hash => {
+          console.log(uint8Array, hash); // The SHA-256 hash of the image data
+        });
+
+      })
+      .catch(error => console.log(error));
+  }
+}
+
+async function sha256(buffer) {
+  const digestBuffer = await window.crypto.subtle.digest('SHA-256', buffer);
+  const hashArray = Array.from(new Uint8Array(digestBuffer));
+  const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
